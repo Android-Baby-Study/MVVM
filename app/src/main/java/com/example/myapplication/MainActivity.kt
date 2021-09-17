3dbfdaefa30fcf896f0281a25206f161137b71c8
jionchu <fn0809@gmail.com>
2021-09-18 01:05:38 +0900

    refactor: 변수명 수정, 클릭 이벤트 코드 간소화

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,34 +8,19 @@ import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModels()
-
     private lateinit var tvNum: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btn_plus).apply {
-            this.setOnClickListener {
-                plusBtnClick()
-            }
-        }
-        findViewById<Button>(R.id.btn_minus).apply {
-            this.setOnClickListener {
-                minusBtnClick()
-            }
-        }
-        findViewById<Button>(R.id.btn_reset).apply {
-            this.setOnClickListener {
-                resetBtnClick()
-            }
-        }
+        findViewById<Button>(R.id.btn_plus).setOnClickListener { plusBtnClick() }
+        findViewById<Button>(R.id.btn_minus).setOnClickListener { minusBtnClick() }
+        findViewById<Button>(R.id.btn_reset).setOnClickListener { resetBtnClick() }
 
         tvNum = findViewById(R.id.tv_num)
 
-        mainViewModel.a.observe(this) { number ->
-            tvNum.text = "$number"
-        }
+        mainViewModel.num.observe(this) { tvNum.text = "$it" }
     }
 
     private fun plusBtnClick() {
--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -5,32 +5,27 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 interface IMainViewModel {
-    val a: LiveData<Int>
+    val num: LiveData<Int>
 
     fun plusNumber()
     fun minusNumber()
     fun resetNumber()
-    fun printNumber(): String
 }
 
 class MainViewModel : IMainViewModel, ViewModel() {
-    private val _a: MutableLiveData<Int> = MutableLiveData(0)
-    override val a: LiveData<Int>
-        get() = _a
+    private val _num: MutableLiveData<Int> = MutableLiveData(0)
+    override val num: LiveData<Int>
+        get() = _num
 
     override fun plusNumber() {
-        _a.value = (a.value ?: 0) + 1
+        _num.value = (num.value ?: 0) + 1
     }
 
     override fun minusNumber() {
-        _a.value = (a.value ?: 0) - 1
+        _num.value = (num.value ?: 0) - 1
     }
 
     override fun resetNumber() {
-        _a.value = 0
-    }
-
-    override fun printNumber(): String {
-        return (a.value ?: 0).toString()
+        _num.value = 0
     }
 }
